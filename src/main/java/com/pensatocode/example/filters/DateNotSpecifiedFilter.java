46b682895bb42b90b4064a0a8643f3005138538c
Alex M <apmagalhaes@ciasc.sc.gov.br>
2023-05-24 08:37:34 +0200

    Fix imports

--- src/main/java/com/pensatocode/example/filters/DateNotSpecifiedFilter.java
+++ src/main/java/com/pensatocode/example/filters/DateNotSpecifiedFilter.java
@@ -3,11 +3,9 @@ package com.pensatocode.example.filters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 @Provider
